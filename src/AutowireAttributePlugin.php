db34d9bc9e7f39b9e383144975ca6b1f20c7fe91
Martin René Sørensen <git@sicet7.com>
2023-04-15 22:58:21 +0200

    Changed plugin to only expect Roave reflector interface.

--- src/AutowireAttributePlugin.php
+++ src/AutowireAttributePlugin.php
@@ -2,28 +2,18 @@
 
 namespace Sicet7\AutowireAttribute;
 
-use Roave\BetterReflection\BetterReflection;
-use Roave\BetterReflection\Reflector\DefaultReflector;
 use Roave\BetterReflection\Reflector\Reflector as ReflectorInterface;
-use Roave\BetterReflection\SourceLocator\Type\DirectoriesSourceLocator;
 use Sicet7\AutowireAttribute\Attributes\Autowire;
 use Sicet7\Plugin\Container\Interfaces\PluginInterface;
 use Sicet7\Plugin\Container\MutableDefinitionSourceHelper;
 
 class AutowireAttributePlugin implements PluginInterface
 {
-    private readonly ReflectorInterface $reflector;
-
     /**
      * @param string ...$directories
      */
-    public function __construct(string ...$directories)
+    public function __construct(private readonly ReflectorInterface $reflector)
     {
-        $sourceLocator = new DirectoriesSourceLocator(
-            $directories,
-            (new BetterReflection())->astLocator()
-        );
-        $this->reflector = new DefaultReflector($sourceLocator);
     }
 
     /**
